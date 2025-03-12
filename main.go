996c30dfe52d88a17eccd2c98b34097c77339ec5
andriy8075 <andriy8075@gmail.com>
2025-03-12 19:13:08 +0200

    change implementation

--- main.go
+++ main.go
@@ -7,218 +7,101 @@ import (
 
 type Character rune
 
-type Node struct {
-	value Character
-	prev  *Node
-	next  *Node
-}
-
 type List struct {
-	head *Node
-	tail *Node
-	size int
+	elements []Character
 }
 
 func NewList() *List {
-	return &List{}
+	return &List{elements: make([]Character, 0)}
 }
 
 func (list *List) Length() int {
-	return list.size
+	return len(list.elements)
 }
 
 func (list *List) Append(element Character) {
-	newNode := &Node{value: element, prev: list.tail}
-
-	if list.tail == nil {
-		list.head = newNode
-	} else {
-		list.tail.next = newNode
-	}
-
-	list.tail = newNode
-	list.size++
+	list.elements = append(list.elements, element)
 }
 
 func (list *List) Insert(element Character, index int) error {
-	if index < 0 || index > list.size {
+	if index < 0 || index > len(list.elements) {
 		return errors.New("індекс поза межами діапазону")
 	}
 
-	if index == list.size {
-		list.Append(element)
-		return nil
-	}
-
-	newNode := &Node{value: element}
-	if index == 0 {
-		newNode.next = list.head
-		list.head.prev = newNode
-		list.head = newNode
-	} else {
-		current := list.head
-		for i := 0; i < index-1; i++ {
-			current = current.next
-		}
-		newNode.next = current.next
-		newNode.prev = current
-		current.next.prev = newNode
-		current.next = newNode
-	}
-
-	list.size++
+	list.elements = append(list.elements, 0)
+	copy(list.elements[index+1:], list.elements[index:])
+	list.elements[index] = element
 	return nil
 }
 
 func (list *List) Delete(index int) (Character, error) {
-	if index < 0 || index >= list.size {
+	if index < 0 || index >= len(list.elements) {
 		return 0, errors.New("індекс поза межами діапазону")
 	}
 
-	var value Character
-	if list.size == 1 {
-		value = list.head.value
-		list.head = nil
-		list.tail = nil
-	} else if index == 0 {
-		value = list.head.value
-		list.head = list.head.next
-		list.head.prev = nil
-	} else if index == list.size-1 {
-		value = list.tail.value
-		list.tail = list.tail.prev
-		list.tail.next = nil
-	} else {
-		current := list.head
-		for i := 0; i < index; i++ {
-			current = current.next
-		}
-		value = current.value
-		current.prev.next = current.next
-		current.next.prev = current.prev
-	}
-
-	list.size--
+	value := list.elements[index]
+	list.elements = append(list.elements[:index], list.elements[index+1:]...)
 	return value, nil
 }
 
 func (list *List) DeleteAll(element Character) {
-	current := list.head
-	for current != nil {
-		if current.value == element {
-			if current.prev == nil {
-				list.head = current.next
-				if list.head != nil {
-					list.head.prev = nil
-				}
-			} else {
-				current.prev.next = current.next
-			}
-
-			if current.next == nil {
-				list.tail = current.prev
-				if list.tail != nil {
-					list.tail.next = nil
-				}
-			} else {
-				current.next.prev = current.prev
-			}
-
-			list.size--
+	newElements := make([]Character, 0)
+	for _, v := range list.elements {
+		if v != element {
+			newElements = append(newElements, v)
 		}
-		current = current.next
 	}
+	list.elements = newElements
 }
 
 func (list *List) Get(index int) (Character, error) {
-	if index < 0 || index >= list.size {
+	if index < 0 || index >= len(list.elements) {
 		return 0, errors.New("індекс поза межами діапазону")
 	}
-
-	current := list.head
-	for i := 0; i < index; i++ {
-		current = current.next
-	}
-
-	return current.value, nil
+	return list.elements[index], nil
 }
 
 func (list *List) Clone() *List {
 	newList := NewList()
-	current := list.head
-	for current != nil {
-		newList.Append(current.value)
-		current = current.next
-	}
+	newList.elements = append(newList.elements, list.elements...)
 	return newList
 }
 
 func (list *List) Reverse() {
-	current := list.head
-	list.tail = current
-	var temp *Node
-
-	for current != nil {
-		temp = current.prev
-		current.prev = current.next
-		current.next = temp
-		current = current.prev
-	}
-
-	if temp != nil {
-		list.head = temp.prev
-
+	for i, j := 0, len(list.elements)-1; i < j; i, j = i+1, j-1 {
+		list.elements[i], list.elements[j] = list.elements[j], list.elements[i]
 	}
 }
 
 func (list *List) FindFirst(element Character) int {
-	current := list.head
-	index := 0
-
-	for current != nil {
-		if current.value == element {
-			return index
+	for i, v := range list.elements {
+		if v == element {
+			return i
 		}
-		current = current.next
-		index++
 	}
-
 	return -1
 }
 
 func (list *List) FindLast(element Character) int {
-	current := list.tail
-	index := list.size - 1
-	for current != nil {
-		if current.value == element {
-			return index
+	for i := len(list.elements) - 1; i >= 0; i-- {
+		if list.elements[i] == element {
+			return i
 		}
-		current = current.prev
-		index--
 	}
-
 	return -1
 }
 
 func (list *List) Clear() {
-	list.head = nil
-	list.tail = nil
-	list.size = 0
+	list.elements = make([]Character, 0)
 }
 
 func (list *List) Extend(other *List) {
-	current := other.head
-	for current != nil {
-		list.Append(current.value)
-		current = current.next
-	}
+	list.elements = append(list.elements, other.elements...)
 }
 
 func (list *List) Print() {
-	current := list.head
-	for current != nil {
-		fmt.Printf("%c ", current.value)
-		current = current.next
+	for _, v := range list.elements {
+		fmt.Printf("%c ", v)
 	}
 	fmt.Println()
 }
