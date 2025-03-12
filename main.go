a3df8e30fbac6e63b5d2f3101a3a62afba6d1d4e
andriy8075 <andriy8075@gmail.com>
2025-03-12 18:51:05 +0200

    break Clone method

--- main.go
+++ main.go
@@ -145,11 +145,6 @@ func (dll *List) Get(index int) (Character, error) {
 
 func (dll *List) Clone() *List {
 	newList := NewList()
-	current := dll.head
-	for current != nil {
-		newList.Append(current.value)
-		current = current.next
-	}
 	return newList
 }
 
